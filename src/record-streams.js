c9650d4de88c94df0c292f844691120f0d00a6ce
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-19 22:15:33 -0800

    Prepare to use js

--- src/record-streams.js
+++ src/record-streams.js
@@ -1,25 +1,24 @@
+import sampleCombine from 'xstream/extra/sampleCombine';
+
 function recordStream (streamInfo, time$) {
   const recordedStream = streamInfo.stream
-    .withLatestFrom(time$, (ev, time) => ({
+    .compose(sampleCombine(time$)).map(([ev, time]) => ({
       timestamp: time, value: ev
     }))
-    .scan((events, newEvent) => {
+    .fold((events, newEvent) => {
       const newEvents = events.concat([newEvent]);
 
       newEvents.label = streamInfo.label;
       newEvents.options = {feature: streamInfo.feature || false};
 
       return newEvents;
-    }, []
-  ).share().startWith([]);
+    }, []);
 
   recordedStream.label = streamInfo.label;
 
   return recordedStream;
 }
 
-function recordStreams (streams, time$) {
+export default function recordStreams (streams, time$) {
   return streams.map(streamInfo => recordStream(streamInfo, time$));
 }
-
-module.exports = recordStreams;
